ce1c7e3063deaa1cc1b56fb00691272fb7a27b18
karintomania <19652340+karintomania@users.noreply.github.com>
2025-04-30 07:29:58 +0100

    tweet skelton

--- external/twitter.go
+++ external/twitter.go
@@ -1,1 +1,31 @@
 package external
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func Post() {
+
+	url := "https://api.twitter.com/2/tweets"
+
+	payload := strings.NewReader(`{
+	"text": "Learn how to use the user Tweet timeline and user mention timeline endpoints in the X API v2 to explore Tweetu"
+}`)
+
+	req, _ := http.NewRequest("POST", url, payload)
+
+	req.Header.Add("Authorization", "Bearer <token>")
+	req.Header.Add("Content-Type", "application/json")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+
+}
--- external/twitter_test.go
+++ external/twitter_test.go
@@ -1,1 +1,12 @@
 package external
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPost(t *testing.T) {
+	// Post()
+	require.True(t, true)
+}
